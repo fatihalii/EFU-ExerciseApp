d6bc939f9b2320b793ed467bf4e3f38bedaf1291
Fatih Ali <fatihali@stu.aydin.edu.tr>
2022-05-05 16:54:16 +0300

    Major changes in the motivation screen

--- components/screens/Motivation.js
+++ components/screens/Motivation.js
@@ -7,6 +7,8 @@ import {
   Dimensions,
 } from "react-native";
 
+import MapView from "react-native-maps";
+
 const images = [
   "https://imgyukle.com/f/2022/04/17/RuThDM.png",
   "https://imgyukle.com/f/2022/04/17/RuTBrn.png",
@@ -20,26 +22,40 @@ const height = width;
 const Motivation = () => {
   return (
     <View style={{ backgroundColor: "#06223d", flex: 1 }}>
-      <View style={styles.viewContainer}>
-        <View style={style.container}>
-          <ScrollView
-            pagingEnabled
-            horizontal
-            showsHorizontalScrollIndicator={false}
-            style={style.Scrollview}
-          >
-            {images.map((image, index) => (
-              <Image
-                key={index}
-                source={{
-                  uri: image,
-                }}
-                style={style.image}
-              />
-            ))}
-          </ScrollView>
+      <ScrollView>
+        <View style={styles.viewContainer}>
+          <View style={styles.container}>
+            <ScrollView
+              pagingEnabled
+              horizontal
+              showsHorizontalScrollIndicator={false}
+              style={styles.Scrollview}
+            >
+              {images.map((image, index) => (
+                <Image
+                  key={index}
+                  source={{
+                    uri: image,
+                  }}
+                  style={styles.image}
+                />
+              ))}
+            </ScrollView>
+          </View>
         </View>
-      </View>
+        <View>
+          <Text style={styles.mapHeader}>NEARBY GYMS</Text>
+        </View>
+        <View style={styles.mapper}>
+          <MapView
+            //region={}
+            provider={MapView.PROVIDER_GOOGLE}
+            showsUserLocation={true}
+            userLocationPriority={"balanced"}
+            style={styles.map}
+          />
+        </View>
+      </ScrollView>
     </View>
   );
 };
@@ -47,17 +63,22 @@ const Motivation = () => {
 export default Motivation;
 
 const styles = StyleSheet.create({
+  mapHeader: {
+    textAlign: "center",
+    color: "white",
+    fontSize: 35,
+    marginTop: 60,
+  },
+
   viewContainer: {
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
     backgroundColor: "#06223d",
   },
-});
 
-const style = StyleSheet.create({
   container: {
-    marginTop: 50,
+    marginTop: 85,
     width,
     height,
     shadowColor: "#000",
@@ -66,6 +87,7 @@ const style = StyleSheet.create({
     shadowRadius: 20,
     elevation: 5,
   },
+
   Scrollview: { width, height },
   image: { width, height, resizeMode: "cover" },
   pagination: {
@@ -73,5 +95,22 @@ const style = StyleSheet.create({
     position: "absolute",
     bottom: 0,
     alignSelf: "center",
+  },
+
+  mapper: {
+    paddingLeft: 20,
+    marginTop: 20,
+    width,
+    height,
+    shadowColor: "#000",
+    shadowOffset: { width: 2, height: 2 },
+    shadowOpacity: 0.8,
+    shadowRadius: 20,
+    elevation: 5,
+  },
+
+  map: {
+    width: 373,
+    height: 350,
   },
 });
