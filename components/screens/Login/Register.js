bd462ef741fc4c2bb77da932978d815f8c50d39d
Fatih Ali <fatihali@stu.aydin.edu.tr>
2022-05-19 00:45:23 +0300

    register page designed.

--- components/screens/Login/Register.js
+++ components/screens/Login/Register.js
@@ -4,12 +4,64 @@ import Colors from "../../../constants/Colors";
 const Register = () => {
   return (
     <View style={styles.pageContainer}>
-      <TextInput
-        style={styles.nameInput}
-        placeholder="Name"
-        placeholderTextColor={Colors.secondary100}
-        textAlign={"center"}
-      />
+      <Text style={styles.headerText}>Create account</Text>
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.nameInput}
+          placeholder="Full Name"
+          placeholderTextColor={Colors.secondary100}
+          textAlign={"center"}
+        />
+        <TextInput
+          style={styles.nameInput}
+          placeholder="E-mail"
+          placeholderTextColor={Colors.secondary100}
+          textAlign={"center"}
+        />
+        <TextInput
+          style={styles.nameInput}
+          placeholder="Password"
+          placeholderTextColor={Colors.secondary100}
+          textAlign={"center"}
+          secureTextEntry={'true'}
+        />
+        <TextInput
+          style={styles.nameInput}
+          placeholder="Confirm Password"
+          placeholderTextColor={Colors.secondary100}
+          textAlign={"center"}
+          secureTextEntry={'true'}
+        />
+        <TextInput
+          style={styles.smallInput}
+          placeholder="Age"
+          keyboardType="number-pad"
+          placeholderTextColor={Colors.secondary100}
+          textAlign={"center"}
+        />
+        <TextInput
+          style={styles.smallInput}
+          placeholder="Height"
+          placeholderTextColor={Colors.secondary100}
+          textAlign={"center"}
+        />
+        <TextInput
+          style={styles.smallInput}
+          placeholder="Weight"
+          placeholderTextColor={Colors.secondary100}
+          textAlign={"center"}
+        />
+        <TextInput
+          style={styles.smallInput}
+          placeholder="Gender"
+          placeholderTextColor={Colors.secondary100}
+          textAlign={"center"}
+        />
+      </View>
+
+      <Pressable style={styles.button} >
+        <Text style={styles.buttonText} >Register</Text>
+      </Pressable>
     </View>
   );
 };
@@ -21,13 +73,54 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: Colors.primary200,
     alignItems: "center",
-    justifyContent: "center",
+    justifyContent: "flex-start",
+  },
+  headerText: {
+    color: "#FFFFFF",
+    fontSize: 35,
+    marginTop:25,
+    marginBottom: 25,
+  },
+  inputContainer:{
+    flexDirection:'row',
+    flexWrap:'wrap',
+    justifyContent:'center',
+    alignItems:'center',
+    marginTop:30,
+    marginBottom:50
   },
   nameInput: {
-    width: 90,
-    height: 40,
+    width: 300,
+    height: 45,
     borderWidth: 1,
     borderRadius: 5,
     borderColor: Colors.secondary100,
+    color: "#FFFFFF",
+    marginVertical:13,
+    marginHorizontal:5
   },
+  smallInput: {
+    width: 145,
+    height: 45,
+    borderWidth: 1,
+    borderRadius: 5,
+    borderColor: Colors.secondary100,
+    color: "#FFFFFF",
+    marginVertical:13,
+    marginHorizontal:5
+  },
+  button: {
+    borderColor: "white",
+    backgroundColor: Colors.secondary100,
+    width: 100,
+    height: 40,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 10,
+    fontSize: 50,
+    textAlign: "center",
+  },
+  buttonText:{
+    color:Colors.primary200
+  }
 });
